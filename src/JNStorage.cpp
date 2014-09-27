c7a6a01940f3ef51c67a436077787aba09f51bc3
Giridhar Addepalli <gaddepalli@walmartlabs.com>
2014-09-27 12:44:41 +0530

    cleaned up analyzeStorage function inside JNStorage

--- src/JNStorage.cpp
+++ src/JNStorage.cpp
@@ -71,9 +71,6 @@ JNStorage::format(const NamespaceInfo& nsInfo) {
     }
     LOG.info("Created paxos dir: %s", getPaxosDir().c_str());
 
-    // TODO :As part of analyze storage , once the storagestate is normal , they are reading back again properties from VERSION file
-    // To me it looks redundant. Hence skipping analyzeStorage for now
-    // Will revisit this decision
     if(analyzeStorage() != 0 ) {
         return -1;
     }
@@ -105,38 +102,17 @@ JNStorage::checkConsistentNamespace(NamespaceInfo nsInfo) {
 int
 JNStorage::analyzeStorage() {
     bool storagedir_exists = false;
-
-    try { // check that storage exists
-        storagedir_exists = boost::filesystem::exists(logDir.c_str());
-    }catch (const boost::filesystem::filesystem_error& ex){
-        LOG.error("%s \n", ex.what());
+    // check that storage exists
+    if(dir_exists(logDir, storagedir_exists) != 0 ) {
         return -1;
     }
+
     if (!storagedir_exists) {
       // storage directory does not exist
-     // if (startOpt != StartupOption.FORMAT) {
         LOG.info("Storage directory %s does not exist", logDir.c_str());
         state = NON_EXISTENT;
         return 0;
-     // }
-//      LOG.info(rootPath + " does not exist. Creating ...");
-//      if (!root.mkdirs())
-//        throw new IOException("Cannot create directory " + rootPath);
     }
-    // or is inaccessible
-//    if (!root.isDirectory()) {
-//      LOG.warn(rootPath + "is not a directory");
-//      return StorageState.NON_EXISTENT;
-//    }
-//    if (!FileUtil.canWrite(root)) {
-//      LOG.warn("Cannot access storage directory " + rootPath);
-//      return StorageState.NON_EXISTENT;
-//    }
-//    } catch(SecurityException ex) {
-//    LOG.warn("Cannot access storage directory " + rootPath, ex);
-//    return StorageState.NON_EXISTENT;
-//    }
-
 //    this.lock(); // lock storage if it exists
 //
     // check whether current directory is valid
@@ -154,48 +130,6 @@ JNStorage::analyzeStorage() {
 
     state = NOT_FORMATTED;
     return 0;
-//    }
-//
-//    if ((hasPreviousTmp?1:0) + (hasRemovedTmp?1:0)
-//      + (hasFinalizedTmp?1:0) + (hasCheckpointTmp?1:0) > 1)
-//    // more than one temp dirs
-//    throw new InconsistentFSStateException(root,
-//                                           "too many temporary directories.");
-//
-//    // # of temp dirs == 1 should either recover or complete a transition
-//    if (hasCheckpointTmp) {
-//    return hasCurrent ? StorageState.COMPLETE_CHECKPOINT
-//                      : StorageState.RECOVER_CHECKPOINT;
-//    }
-//
-//    if (hasFinalizedTmp) {
-//    if (hasPrevious)
-//      throw new InconsistentFSStateException(root,
-//                                             STORAGE_DIR_PREVIOUS + " and " + STORAGE_TMP_FINALIZED
-//                                             + "cannot exist together.");
-//    return StorageState.COMPLETE_FINALIZE;
-//    }
-//
-//    if (hasPreviousTmp) {
-//    if (hasPrevious)
-//      throw new InconsistentFSStateException(root,
-//                                             STORAGE_DIR_PREVIOUS + " and " + STORAGE_TMP_PREVIOUS
-//                                             + " cannot exist together.");
-//    if (hasCurrent)
-//      return StorageState.COMPLETE_UPGRADE;
-//    return StorageState.RECOVER_UPGRADE;
-//    }
-//
-//    assert hasRemovedTmp : "hasRemovedTmp must be true";
-//    if (!(hasCurrent ^ hasPrevious))
-//    throw new InconsistentFSStateException(root,
-//                                           "one and only one directory " + STORAGE_DIR_CURRENT
-//                                           + " or " + STORAGE_DIR_PREVIOUS
-//                                           + " must be present when " + STORAGE_TMP_REMOVED
-//                                           + " exists.");
-//    if (hasCurrent)
-//    return StorageState.COMPLETE_ROLLBACK;
-//    return StorageState.RECOVER_ROLLBACK;
 }
 
 } /* namespace JournalServiceServer */
